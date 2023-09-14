cc448dbd773afcc8cfbe1ab56878e1b57426fe01
Max Edlund <maxedlund88@gmail.com>
2023-09-14 23:28:02 +0200

    add article interface

---
+++ src/models/IArticle.ts
@@ -0,0 +1,11 @@
+export interface IArticle {
+  source: {
+    id: string;
+    name: string;
+  };
+  url: string;
+  author: string;
+  title: string;
+  description: string;
+  content: string;
+}
